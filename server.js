a4f2d26fc8e067a9b11fd817d9b02087fcf80336
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-03-16 17:26:51 +0100

    call the html file

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
 });
 
 app.listen(3000, () => {
