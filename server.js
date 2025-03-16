d2180a95a538628a8cf53cc3d9632a846ee88405
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-03-16 17:30:28 +0100

    middleware : use static files

--- server.js
+++ server.js
@@ -2,13 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
-});
+//middleware : use static files
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
