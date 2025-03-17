b083302d9f2f5bc5cf59f0772f036f6ea9b8fc0e
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-03-18 00:26:07 +0100

    Api get all posts

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+let psosts = [
+  { id: 1, title: "First post" },
+  { id: 2, title: "Second post" },
+  { id: 3, title: "Third post" },
+];
+
+// get all posts
+app.get("/api/posts", (req, res) => {
+  res.json(psosts);
+});
 
 //middleware : use static files
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
