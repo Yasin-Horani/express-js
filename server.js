fbdaeb4afa06212608369d449ce0ae636725236c
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-03-18 00:49:08 +0100

    server status

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.get("/api/posts", (req, res) => {
 // filter the posts array and find the post with the id
 app.get("/api/post/:id", (req, res) => {
   const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (post) {
+    return res.status(200).json(post);
+  } else {
+    return res.status(404).send("Post not found 404");
+  }
   res.json(posts.filter((post) => post.id === id));
 });
 
