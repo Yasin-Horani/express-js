d537f62acb7461ab3efa3e786d0f8d502fcec3f5
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-03-18 16:28:57 +0100

    display path in server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import posts from "./routes/posts.js";
+import path from "path";
 
+const __dirname = path.resolve();
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
