a8fe043c6412d48e96b2575d217d28fc93257b56
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-03-18 01:04:00 +0100

    using module

---
+++ routes/posts.js
@@ -0,0 +1,28 @@
+import express from "express";
+const router = express.Router();
+
+let posts = [
+  { id: 1, title: "First post" },
+  { id: 2, title: "Second post" },
+  { id: 3, title: "Third post" },
+];
+
+// get all posts
+router.get("/", (req, res) => {
+  res.json(posts);
+});
+
+// get single posts
+
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (post) {
+    return res.status(200).json(post);
+  } else {
+    return res.status(404).send("Post not found 404");
+  }
+  res.json(posts.filter((post) => post.id === id));
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,33 +1,10 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import posts from "./routes/posts.js";
+
 const app = express();
-let posts = [
-  { id: 1, title: "First post" },
-  { id: 2, title: "Second post" },
-  { id: 3, title: "Third post" },
-];
-
-// get all posts
-app.get("/api/posts", (req, res) => {
-  res.json(posts);
-});
-
-// get single posts
-// filter the posts array and find the post with the id
-app.get("/api/post/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-  if (post) {
-    return res.status(200).json(post);
-  } else {
-    return res.status(404).send("Post not found 404");
-  }
-  res.json(posts.filter((post) => post.id === id));
-});
-
-//middleware : use static files
-//app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/posts", posts);
